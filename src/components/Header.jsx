7fb4cb8cf1442785d9a5482dfe5a6697dd819b9d
akash bawantahde <akashbawanthade02@gmail.com>
2025-12-31 20:06:52 +0530

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Header = () => {
+
+    const handleClick = () => {
+        alert("logout")
+    }
+
   return (
-    <div className='flex items-center text-red-900'>
-      <h1>Fainancly</h1>
+    <div className='flex items-center justify-between p-4 bg-gray-100'>
+      <h1 className='text-4xl font-bold text-red-900'>Fainancly</h1>
+      <p onClick={handleClick}>Logout</p>
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from "../components/Header"
+import SignupSignin from "../components/SignupSignin"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+      <Header />
+      <h1 className='flex justify-center items-center h-[90vh] text-3xl'>
+         <SignupSignin />
+      </h1>
+    </div>
   )
 }
 
